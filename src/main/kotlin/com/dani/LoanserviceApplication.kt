7837f51a6e6f27a0a8b99f088423620d7f5167de
DaniBudo <daniel_budo@yahoo.com>
2026-04-21 17:14:01 +0200

    initial commit

---
+++ src/main/kotlin/com/dani/LoanserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.dani
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoanserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LoanserviceApplication>(*args)
+}
